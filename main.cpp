4313cb3107e4ac68d2cbe087eac92b33329922f1
Thales Henrique <thales.henrique@engenharia.ufjf.br>
2020-10-11 13:05:39 -0300

    Less false positives

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/core/core.hpp"
-#include "img_processing.h"
 #include "clustering.h"
 #include <iostream>
 
@@ -26,46 +25,41 @@ int main(int argc, char **argv)
     CascadeClassifier cascade;
     cascade.load("cascade.xml");
     loadFile("campo.txt");
+
+    Mat frame_gray;
+    //Mat frame_hsv;
+    std::vector<Rect> balls;
+    //Mat hsv_channels[3];
+    Mat resized_frame(Size(320, 240), CV_8UC3);
+    Mat frame;
+
+
     while (1)
     {
-        Mat frame;
         // Capture frame-by-frame
         cap >> frame;
 
-        Mat resized_frame(Size(320, 240), CV_8UC3);
         resize(frame, resized_frame, resized_frame.size());
         frame = resized_frame.clone();
 
         clustering(frame);
 
-        std::vector<Rect> balls;
-        Mat frame_gray, frame_hsv;
-        Mat hsv_channels[3];
 
+        //cvtColor(frame, frame_hsv, CV_BGR2HSV);
 
-        cvtColor(frame, frame_hsv, CV_BGR2HSV);
+        //imshow("hsv", frame_hsv);
 
-        Mat frame_lines, frame_posts, frame_ball;
-	    vector<Point> goalRoots;
-	    double hor_hist[frame_hsv.cols];
-	    int ver_hist[frame_hsv.rows];
-
-        remove_background(frame_hsv,frame_lines,frame_posts,frame_ball,goalRoots,hor_hist,ver_hist);
-        imshow("hsv", frame_hsv);
-
-        //binarize(frame_hsv);
-
-        split(frame_hsv, hsv_channels);
-        frame_gray = hsv_channels[2];
-        imshow("gray", frame_gray);
-        //cvtColor(frame, frame_gray2, COLOR_BGR2GRAY);
-        //imshow("gra_2",frame_gray2);
+        //split(frame_hsv, hsv_channels);
+        //frame_gray = hsv_channels[2];
+        //imshow("gray", frame_gray);
+        cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+        imshow("gray",frame_gray);
         //inRange(frame, Scalar(38, 50, 50), Scalar(75, 255, 255), frame_gray); //problemas aqui
 
 
-        cvtColor(frame_hsv, frame, CV_HSV2BGR);
-        //cascade.detectMultiScale(frame_gray, balls, 1.3, 5, 8, Size(16, 16)); // Rinobot
-        cascade.detectMultiScale(frame_gray, balls, 1.1, 5, 8, Size(16, 16));
+        //cvtColor(frame_hsv, frame, CV_HSV2BGR);
+        cascade.detectMultiScale(frame_gray, balls, 1.3, 5, 8, Size(16, 16)); // Rinobot
+        //cascade.detectMultiScale(frame_gray, balls, 1.1, 5, 8, Size(16, 16));
 
         if (balls.size() == 0)
         {
